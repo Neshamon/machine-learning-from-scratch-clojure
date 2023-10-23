0ba49f7e27f2430965dc92a568837b084c2a7df6
neshamon <jmmatthews@proton.me>
2023-10-23 17:11:22 -0500

    Implement the basic regression.

--- src/ml_from_scratch/core.clj
+++ src/ml_from_scratch/core.clj
@@ -5,3 +5,10 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn basic-regression [weights inputs]
+  (+ (first weights)
+     (* (second weights) (first inputs))
+     (* (last weights) (last inputs))))
+
+(basic-regression [34 0.39 0.33] [165 185])
