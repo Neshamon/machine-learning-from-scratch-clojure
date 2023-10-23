3e741d83d3b2cd7b1ebbf7e9c171b2359729f6ee
neshamon <jmmatthews@proton.me>
2023-10-23 17:59:15 -0500

    Annotate code

--- src/ml_from_scratch/core.clj
+++ src/ml_from_scratch/core.clj
@@ -6,9 +6,17 @@
   [& args]
   (println "Hello, World!"))
 
-(defn basic-regression [weights inputs]
-  (+ (first weights)
-     (* (second weights) (first inputs))
-     (* (last weights) (last inputs))))
+(def mother-height 165)
 
-(basic-regression [34 0.39 0.33] [165 185])
+(def father-height 185)
+
+(defn child-height-regression
+  "This function performs basic linear regression on a the heights
+  of a child's parents with the given weights. It will return a string
+  which is the predicted height of the child"
+  [weights parent-height]
+  (apply str (concat (str (+ (first weights)
+                             (* (second weights) (first parent-height))
+                             (* (last weights) (last parent-height)))) " cm")))
+
+(child-height-regression [34 0.39 0.33] [mother-height father-height]) ;; => 159.4 cm
