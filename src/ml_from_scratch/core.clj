5e394c2ab1383efe0cac783118e3b2777f450738
neshamon <jmmatthews@proton.me>
2023-10-23 19:28:13 -0500

    Create basic regression and least square loss and annotate.

--- src/ml_from_scratch/core.clj
+++ src/ml_from_scratch/core.clj
@@ -19,4 +19,32 @@
                              (* (second weights) (first parent-height))
                              (* (last weights) (last parent-height)))) " cm")))
 
+(defn basic-regression
+  "This function is a more abstract version of child-height regression.
+  It takes weights, inputs, and an optional unit and then returns
+  a prediction based off of the data.
+
+  Merged bias is a variable that makes it easier to calculate what
+  the prediction may be by allowing the first weight to remain itself
+  while the mapping function is applied to the rest of the data.
+  It does this by adding a 1 to the beginning of the input data structure
+  and coerces it to a vector.
+
+  Merged bias is then used in the place of inputs when calculating
+  the prediction. This calcuation is done by multiplying each value
+  in the sequences weights and merged bias, and then is summed up by
+  the reducing function to emulate the function:
+  f(x,w) = w0 * x0 + w1 * x1 + ... wn * xn
+  "
+  [weights inputs & unit]
+  (let [merged-bias (vec (cons 1 inputs))]
+    (if unit
+      (apply str (concat (str (reduce + (map #(* %1 %2) weights merged-bias))) unit))
+      (reduce + (map #(* %1 %2) weights merged-bias)))))
+
 (child-height-regression [34 0.39 0.33] [mother-height father-height]) ;; => 159.4 cm
+
+(basic-regression [34 0.39 0.33] [mother-height father-height]) ;; => 159.4 cm
+
+(defn least-square-loss [weights inputs prediction]
+  (/ (reduce + (reduce * (repeat 2 (- prediction (map #(* %1 %2) weights inputs))))) 2))
